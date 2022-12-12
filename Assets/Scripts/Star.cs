af041bda0a994f3026bdd82ebd96bdf3307f65ad
ctcticxh <3574736597@qq.com>
2022-12-12 18:40:25 +0800

    添加了阳光消失

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
+  public float duration;//持续时间
+  private float timer;//计时器
+  private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+    anim = GetComponent<Animator>();
+    timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    SwitchAnim();
     }
+
+  void SwitchAnim()
+  {
+    if (anim.GetBool("idle"))
+    {
+      timer += Time.deltaTime;
+      if (timer >= duration)
+      {
+        timer = 0;
+        anim.SetBool("Disappear",true);
+      }
+    }
+  }
+  public void Disappearing()
+  {
+    GameObject.Destroy(gameObject);
+  }
+  private void OnMouseDown()
+  {
+    anim.SetBool("Disappear", true);
+    GameManager.instance.ChangeStarNum(25);
+  }
 }
