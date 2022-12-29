49143d00a8887298ae285eaa89ffe0dc8aa2c712
ctcticxh <3574736597@qq.com>
2022-12-29 13:37:57 +0800

    芜湖

--- Assets/Scripts/base/UIManager.cs
+++ Assets/Scripts/base/UIManager.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
   public static UIManager instance;
+  public Slider slider;
   public Text starNumText;
+  public Text MoraText;
+  public float timer;
     // Start is called before the first frame update
     void Start()
     {
+    timer = 0;
     instance = this;
-    
+    slider.value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
     UIManager.instance.UpdateUI();
+    timer += Time.deltaTime;
+    if (timer >= 10)
+    {
+      slider.value += Time.deltaTime / 20;
     }
+  }
   public void InitUI()//初始化UI
   {
    
@@ -25,9 +34,14 @@ public class UIManager : MonoBehaviour
   public void UpdateUI()//更新UI
   {
     starNumText.text = GameManager.instance.starNum.ToString();
+
   }
   public Vector3 GetSunNumTextPos()
   {
     return starNumText.transform.position;//得到阳光的屏幕坐标
+  }
+  public Vector3 GetMoraNumTextPos()
+  {
+    return MoraText.transform.position;
   }
 }
