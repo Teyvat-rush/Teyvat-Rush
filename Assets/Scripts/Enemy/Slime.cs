37cac1c16b8f482998c326ba474e9fa6fe09f432
ctcticxh <3574736597@qq.com>
2023-01-16 18:47:10 +0800

    修好啦

--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Slime : Enemy
 {
-  private Transform Bornposition;
+  private float BornY;
   public GameObject LeftWing;
   public GameObject RightWing;
+  public bool isAttack;
     // Start is called before the first frame update
    void Start()
     {
-    Bornposition = transform;
+    BornY = transform.position.y;
     anim = GetComponent<Animator>();
     rb = GetComponent<Rigidbody2D>();
     coll = GetComponent<BoxCollider2D>();
@@ -21,6 +22,11 @@ public class Slime : Enemy
     // Update is called once per frame
     void Update()
     {
+    isAttack = false;
+    if(isAttack)
+    {
+      Speed = 0;
+    }
     if (!isDead)
     {
       Movement();
@@ -40,23 +46,16 @@ public class Slime : Enemy
   {
     rb.velocity = new Vector3(-Speed, rb.velocity.y,0);
   }
-  private void OnTriggerEnter2D(Collider2D collision)
-  {
-    if(collision.gameObject.tag=="Plant")
-    {
-      anim.SetBool("Jump", false);
-      anim.SetBool("Attack", true);
-      PauseMove();
-      transform.position = new Vector3(transform.position.x, Bornposition.position.y, transform.position.z);
-    }
-  }
+
   private void OnTriggerStay2D(Collider2D collision)
   {
     if (collision.gameObject.tag == "Plant")
     {
+      isAttack = true;
       anim.SetBool("Jump", false);
       anim.SetBool("Attack", true);
-      PauseMove();
+      Speed = 0;
+      transform.position = new Vector3(transform.position.x, BornY, transform.position.z);
       damageTimer += Time.deltaTime;
       if(damageTimer>=damageInterval)
       {
@@ -65,6 +64,7 @@ public class Slime : Enemy
         float newHealth = plant.ChangeHealth(-damage);
         if (newHealth <= 0)
         {
+          isAttack = false;
           anim.SetBool("Attack", false);
           anim.SetBool("Jump", true);
           Speed = 0.8f;
@@ -76,6 +76,7 @@ public class Slime : Enemy
   {
     if(collision.tag=="Plant")
     {
+      isAttack = false;
       anim.SetBool("Attack", false);
       anim.SetBool("Jump", true);
       ContinueMove();
