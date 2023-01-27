b8ad41d888f7c5c6ef36833fb359c4549998d04b
ctcticxh <3574736597@qq.com>
2023-01-27 16:36:33 +0800

    淦，又无了

--- Assets/Scripts/base/Lines.cs
+++ Assets/Scripts/base/Lines.cs
@@ -12,7 +12,30 @@ public class Lines : MonoBehaviour
   // Start is called before the first frame update
   void Start()
     {
-    
+    if(GameManager.curLevelID>=2)
+    {
+      line0.SetActive(true);
+      line1.SetActive(true);
+      line2.SetActive(true);
+      line3.SetActive(true);
+      line4.SetActive(true);
+    }
+    else if(GameManager.curLevelID>=1)
+    {
+      line0.SetActive(false);
+      line1.SetActive(true);
+      line2.SetActive(true);
+      line3.SetActive(true);
+      line4.SetActive(false);
+    }
+    else
+    {
+      line0.SetActive(false);
+      line1.SetActive(false);
+      line2.SetActive(true);
+      line3.SetActive(false);
+      line4.SetActive(false);
+    }
     }
 
     // Update is called once per frame
