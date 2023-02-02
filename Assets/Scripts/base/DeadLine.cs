fbe167a8c62323f0e59a14d640d419cabcb61be6
ctcticxh <3574736597@qq.com>
2023-02-02 21:27:24 +0800

    好

--- Assets/Scripts/base/DeadLine.cs
+++ Assets/Scripts/base/DeadLine.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DeadLine : MonoBehaviour
 {
   public GameObject panel_Failed;
-    public Sprite image_Enemy;
+    public GameObject image_Enemy;
+  public Text Enemy_Name;
     private void OnTriggerEnter2D(Collider2D collision)
   {
     if(collision.tag=="Enemy")
     {
       panel_Failed.SetActive(true);
       SoundManager.instance.StopBGM();
-            if(collision.gameObject.name=="qqRen")
-            image_Enemy = LibraryOfEnemy.instance.ALLImages[2];
+      image_Enemy.GetComponent<Image>().sprite= LibraryOfEnemy.instance.ALLImages[collision.GetComponent<Enemy>().Enemyid];
+      Enemy_Name.text = LibraryOfEnemy.ALLEnemies[collision.GetComponent<Enemy>().Enemyid].characterName;
+            //if(collision.gameObject.name=="qqRen")
+            //image_Enemy = LibraryOfEnemy.instance.ALLImages[2];
     }
   }
 
