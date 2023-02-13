3f0dc0aec7926465ff713de5a85ea7585490b787
DeliGitee <1765939870@qq.com>
2023-02-13 20:44:01 +0800

    Script_20.2

--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -23,7 +23,7 @@ public class Slime : Enemy
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("史莱姆anim" + anim.GetBool("Attack"));
+        //Debug.Log("史莱姆anim" + anim.GetBool("Attack"));
         isAttack = false;
     if(isAttack)
     {
